66e1d1c54338f0632dc5fa3f53f07cc6bc59d399
Francisco Prado <franciscoprado@csus.edu>
2020-04-23 13:48:24 -0700

    fix autoload issue. If auto load fails play button will be displayed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
 setInterval(refresh,15000 );
 setInterval(refreshRecents, 60000);
+//test code
+
+//-------------test code end
+$('#play').hide();
+var song = new Audio();
+song.src = "https://streamingv2.shoutcast.com/kssu";
+song.volume = 0.7;
+window.onload = playFirst();
+playFirst();
+
+function playFirst(){
+    song.play().catch(error => {
+        // Autoplay was prevented.
+        // Show a "Play" button so that user can start playback.
+        playSong()
+    });
+}
+function stopSong(){
+    song.src = "https://streamingv2.shoutcast.com/kssu";
+    song.play();
+    $('#play').hide();
+    $('#stop').show();
+}
+function playSong(){
+    song.pause();
+    $('#stop').hide();
+    $('#play').show();
+}
+function changeVolume(v_value){
+    song.volume = v_value;
+}
+
 function close_window() {
     close();
 
