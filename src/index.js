77b614ce4f4d2a30f6bb184d6fabd133ee88f733
Francisco Prado <franciscoprado@csus.edu>
2020-04-23 13:23:57 -0700

    Changed background and fixed miniplayer song info

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+setInterval(refresh,15000 );
+setInterval(refreshRecents, 60000);
 function close_window() {
     close();
 
@@ -5,4 +7,11 @@ function close_window() {
 function Open() {
     window.open("https://www.kssu.com", "_blank");
 }
+function refresh() {
+    document.getElementById('iframeid').src = document.getElementById('iframeid').src;
+}
+function refreshRecents() {
+    document.getElementById('myiframe').src = document.getElementById('myiframe').src;
+    console.log("hello");
 
+}
